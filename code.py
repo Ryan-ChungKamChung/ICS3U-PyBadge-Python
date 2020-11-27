15f9f8cc5b221cd35145f378a3d576fe35b1590d
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2020-11-27 16:14:47 -0500

    Created code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# "Hello, World!" on a PyBadge
+
+
+import ugame
+import stage
+
+
+def game_scene():
+    # Main game scene
+    print("Hello, World!")
+
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    game_scene()
