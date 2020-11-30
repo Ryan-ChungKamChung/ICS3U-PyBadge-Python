fe0e3d724ae5742d9f5dffaeaeaf58f7cd7e79c2
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2020-11-30 12:55:46 -0500

    Update code.py

--- Lesson 2/code.py
+++ Lesson 2/code.py
@@ -2,21 +2,20 @@
 
 # Created by Ryan Chung Kam Chung
 # Created in November 2020
-# "Hello, World!" on a PyBadge
-
+# Setting background on the PyBadge
 
 import ugame
 import stage
 
 
 def game_scene():
-    # main game scene
-    
+    # this function is the main game scene
+
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     background = stage.Grid(image_bank_background, 10, 8)
-    
+
     game = stage.Stage(ugame.display, 60)
-    game_layers = [background]
+    game.layers = [background]
     game.render_block()
 
     while True:
