c1f3e2b7cc43362fdebe45303245dd7d9acd9333
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2020-12-06 15:08:10 -0500

    Create constants.py

---
+++ Lesson 5/constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in December 2020
+# Constants file for code.py
+
+# Screen is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
