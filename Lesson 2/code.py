c712533b62e35a20731390ca4b054664af1af679
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-15 10:52:00 -0500

    Update code.py

--- Lesson 2/code.py
+++ Lesson 2/code.py
@@ -1,51 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3	#!/usr/bin/env python3
+
 
 # Created by Ryan Chung Kam Chung
-# Created in January 2021
+# Created in November 2020
 # Setting background on the PyBadge
+
 
 
 # Libraries that will enable us to render and stage assets
 import ugame
 import stage
 
-# Constants file
-import constants
-
 
 def game_scene():
     # this function is the main game scene
 
+
     # Image bank holds all image assets used
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)	
 
-    # Sets background to the 0th image in the image bank
-    # 10x8 grid
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X,
-                              constants.SCREEN_GRID_Y)
-
-    # Sets the floor as the 1st image in the image bank, the walls are still going
-    # to be the 0th image
-    for x_location in range(1, constants.SCREEN_GRID_X - 1):
-        for y_location in range(1, constants.SCREEN_GRID_Y - 1):
-            tile_picked = 1
-            background.tile(x_location, y_location, tile_picked)
-
-    # Creates a stage for the background
-    # Sets frame rate to 60fps
-    game = stage.Stage(ugame.display, constants.FPS)
-    # Sets sprite layers and show up in order
+    game = stage.Stage(ugame.display, 60)
     game.layers = [background]
-
-    # Renders all sprites
-    # Usually you should render background once per scene
     game.render_block()
 
-    # Placeholder for now to extend the scene time
     while True:
         pass
 
-
-# Makes this file run as the main file of the program, and runs game_scene()
 if __name__ == "__main__":
     game_scene()
